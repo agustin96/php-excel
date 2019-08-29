b93bd03dddffc9f70e16073bdb2d733dcef1cb1e
Agustin <agustin.amato96@gmail.com>
2019-08-29 12:55:48 -0300

    automatic commit

--- export_xls.php
+++ export_xls.php
@@ -1,11 +1,40 @@
 <?php
+
 use Slam\Excel\Helper as ExcelHelper;
 
 require __DIR__ . '/vendor/autoload.php';
 
 // Being an Iterator, the data can be any dinamically generated content
 // for example a PDOStatement set on unbuffered query
-$users = new ArrayIterator([
+
+$database = "maricel";
+
+require 'connections/conexion.php';
+
+$registros = $base->query("SELECT
+
+                        xt_cierre1.fecha_real			AS fecha,
+                        SUM(xt_cierre1.total)			AS total,
+
+                        FROM xt_cierre1
+                                        
+                        WHERE id_local=1
+
+                        GROUP BY fecha , id_local
+                        ORDER BY fecha LIMIT 10")->fetchAll(PDO::FETCH_OBJ);
+
+
+foreach ($registros as $objeto) :
+    $fecha = $objeto->fecha;
+    $total = $objeto->total;
+    $data[] = [
+        'column_1' => $fecha,
+        'column_2' => $total,
+        'column_3' => '2017-05-08',
+    ];
+endforeach;
+
+/* $users = new ArrayIterator([
     [
         'column_1' => 'John',
         'column_2' => '123.45',
@@ -16,7 +45,9 @@ $users = new ArrayIterator([
         'column_2' => '4321.09',
         'column_3' => '2018-05-08',
     ],
-]);
+]); */
+
+$users = new ArrayIterator([$data]);
 
 $columnCollection = new ExcelHelper\ColumnCollection([
     new ExcelHelper\Column('column_1',  'User',     10,     new ExcelHelper\CellStyle\Text()),
@@ -37,4 +68,4 @@ $table->setColumnCollection($columnCollection);
 $phpExcel->writeTable($table);
 $phpExcel->close();
 
-readfile($filename);
+echo $filename;
